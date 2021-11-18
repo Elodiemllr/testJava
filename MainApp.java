1ed03f324fde40d2d22fcb65286a760d83d01617
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-11-18 16:17:32 +0100

    explication d'une class en java

---
+++ MainApp.java
@@ -0,0 +1,21 @@
+ //class = entité 
+ // nom du fichier = nom de la class
+
+ class MainApp 
+ {
+     //function à l'interieur d'une class = methode 
+
+     //main = function de départ du programm, obligatoire car il veut demarrer par  celle ci
+     //String[] args  = paramètre/argument qu'on veut passer à notre function, obligatoire de signer 
+     //void = car aucun retour attendu 
+     //static = méthode executable ici
+     /// public car accessble partout, meme en dehors de la classe 
+     // Tout ici est obligatoire,   public static void main(String[] args) pour commencer nos class 
+     public static void main(String[] args)
+     { 
+         //System est une class native de java, lié au system
+         /// out est un attribu, une variable dans une class 
+         // Println est aussi une function disponible qui nous permet d'affficher quelque chose à la sortie (comme console.log), ln = retour à la ligne 
+         System.out.println("Hello world");
+     }
+ }
