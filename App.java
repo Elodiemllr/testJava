601e5ff602461a2e9b01f2af283f805bed46ae65
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-11-24 19:24:00 +0100

    les accesseurs

--- App.java
+++ App.java
@@ -3,9 +3,19 @@ public class App {
     public static void main(String [] args)
     {
         SoftwareRegistration yearActual = new SoftwareRegistration(2020);
+        /*
+        Maintenant je ne pourrais pas modifié l'expiration year car l'acces est privé
         yearActual.expirationYear = 2022; 
-        System.out.println("Valide jusqu'en " +   yearActual.expirationYear);
+        System.out.println("Valide jusqu'en " +   yearActual.expirationYear);*/
 
+        //on va là directement modifier notre setter et le remettre au getter
+
+        //AVANT 
+        //  yearActual.expirationYear(2025);
+        //System.out.println(yearActual.expirationYear );
+        //APRES
+        yearActual.setExpirationYear(2025);
+        System.out.println(yearActual.getExpirationYear());
     }
     
 }
--- SoftwareRegistration.java
+++ SoftwareRegistration.java
@@ -2,7 +2,7 @@
 public class SoftwareRegistration {
 
     //si je ne le met pas "private" je vais pouvoir aller le modifier dans une autre class
-     int expirationYear;
+     private int expirationYear;
 
     //accésseur (geteur, seteur, get set)
     //getters: acceder à un attreibut
@@ -10,9 +10,36 @@ public class SoftwareRegistration {
     // a créer dans la class en question
 
     public SoftwareRegistration(int expiration) {
-this.expirationYear = expiration;
+        this.expirationYear = expiration;
 
-System.out.println("Valide jusqu'en " +  this.expirationYear);
+        System.out.println("Valide jusqu'en " +  this.expirationYear);
     }
-     
+
+    //pour chaque attribut on va creer deux methodes supplémentaire 
+    //c'est pour garder ce principe d'encapsulation des données
+    //on va modifier via nos accesseur (set get)
+
+    //ACCESSEUR
+
+    //getter
+    //en general un getter ne prends pas de parametres
+    //il va retourner notre attrivuts
+    //on 'get' (recupère)  notre int
+    public int getExpirationYear() {
+        return this.expirationYear;
+
+        //on peut interdir la lecture d'un attribut en modifiant le int par void et en faisant aussi un return
+    }
+
+    //setter
+    //le setter ne va rien retourner
+    // pour la modification
+     public void setExpirationYear(int myExpiration) {
+        this.expirationYear = myExpiration;
+
+        //SI ON VEUT BLOQUER LA MODIFE
+        // return;
+        //du coup il ne pourra rien retourner (void)
+
+     }
 }
