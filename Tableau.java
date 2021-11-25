56a8c3d93c8f7e6252f74f63fc65cebf3494c18e
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-11-25 17:38:04 +0100

    tableau à deux dimensions

--- Tableau.java
+++ Tableau.java
@@ -21,6 +21,25 @@ public class Tableau {
         for(int el: tab) {
             System.out.println("chifre " + el  );
         }
+
+        //Tableau à deux dimensions = un tableau ou chacun de ses elements sont aussi des tableaux
+        int[][] tab2 = {{1, 2, 3}, {4, 5, 6} };
+
+        //JE VEUX LE CHIFFRE 5 DU DEUXIEME TABLEAU
+        System.out.println(tab2[1][1]);
+
+        //si on veut boucler il va fallor  eux boucles, une premiere pour parcourire notre premier { } puis une deuxieme pour un tab individuel
+        for(int i = 0; i < tab2.length; i++) {
+            for(int j =0; j < tab2[i].length;  ++j) {
+                System.out.println(tab2[i][j] + " boucle tableau deux dimensions");
+            }
+        }
+
+        for(int[] i : tab2) {
+            for (int number: i) {
+                System.out.println(number + " meilleur boucle");
+        }
     }
+}
     
 }
